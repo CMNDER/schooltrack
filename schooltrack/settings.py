0d1cad87dc9ca6e2bed9cf8a952ba1dbc2c6d4bf
CMNDER <samuelishimwe@gmail.com>
2021-01-05 16:40:39 +0200

    updates

--- schooltrack/settings.py
+++ schooltrack/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,6 +132,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000"
